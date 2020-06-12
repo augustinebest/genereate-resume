96de985808db0ca74c10f55f49967e23080ef30e
Igwe Augustine <augustusbest00@gmail.com>
2020-06-12 14:02:14 +0100

    added the dynamic content

--- utils/templates.js
+++ utils/templates.js
@@ -8,8 +8,8 @@ const template1 = (data) => {
             <h1>${data.firstname} ${data.lastname}</h1>
             <h4 class="light m-m-t-10" style="color:grey; padding-top:5px">
                 ${Object.keys(data.contact)
-            .map(function(key, index) {
-              return ` 
+            .map(function (key, index) {
+                return ` 
                 <span class="" >${data.contact[key]}</span>`;
             })
             .join(" •")}
@@ -20,88 +20,88 @@ const template1 = (data) => {
         <h4 class="">TECHNICAL SKILLS</h4>
         <div class="offset-2">
             ${data.skills.details
-          .map(function(key) {
-            return `
+            .map(function (key) {
+                return `
             <p>
                 <strong>${key.type}:</strong> ${key.items
-              .map(function(key) {
-                return `${key}`;
-              })
-              .join(", ")}`;
-          })
-          .join("</p>")}
+                        .map(function (key) {
+                            return `${key}`;
+                        })
+                        .join(", ")}`;
+            })
+            .join("</p>")}
             </div>
         <h4 class="">PERSONAL PROJECTS</h4>
         <div class="offset-2">
     
     
             ${data.github_projects.items
-          .map(function(key) {
-            return `
+            .map(function (key) {
+                return `
             <p>
                 <strong class="title">${
-            key["project_name"]
-          } ${converter.makeHtml(key["tagline"]).replace(/<(\/)?p([^>]*)>/g, "")}
+                    key["project_name"]
+                    } ${converter.makeHtml(key["tagline"]).replace(/<(\/)?p([^>]*)>/g, "")}
                 </strong>
             </p>
             <div class="offset-2 p"> ${key["description"][0]} Technologies: ${key[
-              "technology_used"
-            ].tools
-              .map(function(item) {
-                return item;
-              })
-              .join(", ")}
+                        "technology_used"
+                    ].tools
+                        .map(function (item) {
+                            return item;
+                        })
+                        .join(", ")}
             </div> `;
-          })
-          .join("")}
+            })
+            .join("")}
     
     
         </div>
         <h4 class="">OTHER PROJECTS</h4>
     
         ${data.other_projects.items
-        .map(function(key) {
-          return `<div class="offset-2">
+            .map(function (key) {
+                return `<div class="offset-2">
         <p>
             <strong class="title">${key["headline"]}</strong>
         </p>
         <div class="offset-2 p"> ${key["points"][0]} Technologies: ${key[
-            "technology_used"
-          ].tools
-            .map(function(item) {
-              return item;
-            })
-            .join(", ")}
+                        "technology_used"
+                    ].tools
+                        .map(function (item) {
+                            return item;
+                        })
+                        .join(", ")}
         </div></div>`;
-        })
-        .join("")}
+            })
+            .join("")}
         <h4 class="">PROFESSIONAL EXPERIENCE</h4>
         <div class="offset-2">
             <p>
-                <strong class="title">${data.work_experience.items.map(function(key) {
-            return `${key["title"]}, ${key["organisation"]}, ${key["location"]}</b>
+                <strong class="title">${data.work_experience.items.map(function (key) {
+                return `${key["title"]}, ${key["organisation"]}, ${key["location"]}</b>
                     <span class="pull-right">${key["from"]} - ${key["to"]}</span>
                 </strong>
             </p>
             <div class="offset-2 p">${key["details"][0]} Technologies: ${key[
-              "technology_used"
-            ].tools
-              .map(function(item) {
-                return item;
-              })
-              .join(", ")}
+                        "technology_used"
+                    ].tools
+                        .map(function (item) {
+                            return item;
+                        })
+                        .join(", ")}
             </div>
         </div>`;
-          })}
+            })}
     
     
         <h4 class="">INVOLVEMENTS</h4>
         <div class="">
             <ul class="boxed-list offset-2 p">
-                ${data.involvement.organizations.map(function(key) {
-            return `
+                ${data.involvement.organizations.map(function (key) {
+                return `
                 <li class="offset-2" style="text-decoration-style:disc"> ${key} </li>`;
-          })}
+            })}
             </ul>
         </div>
         <h4 class="">EDUCATION</h4>
@@ -119,7 +119,7 @@ const template1 = (data) => {
                 <tbody>
     
                     <tr>
-                        ${data.education.schools.map(function(key) {
+                        ${data.education.schools.map(function (key) {
                 return `
                         <td>${key["degree"]}</td>
                         <td>${key["major"]}</td>
@@ -127,7 +127,7 @@ const template1 = (data) => {
                         <td>${key["graduation_year"]}</td>
     
                         `;
-              })}
+            })}
                     </tr>
                 </tbody>
     
@@ -137,15 +137,15 @@ const template1 = (data) => {
         <h4 class="">RESEARCH EXPERIENCE</h4>
         
         ${data.research_experience.items
-          .map(function(key) {
-            return `<div class="offset-2">
+            .map(function (key) {
+                return `<div class="offset-2">
         <p>
             <strong class="title">${key["title"]}- ${key["organisation"]}</strong>
         </p>
         <div class="offset-2 p"> ${key["points"][0]}
         </div></div>`;
-          })
-          .join("")}
+            })
+            .join("")}
     
     </body>
     `;
@@ -157,78 +157,91 @@ const template2 = (data) => {
     <body class="boxed">
     <div class="main">
         <div class="left_bar">
-            <h1>JOHN JAMES DOE</h1>
-            <h4 class="light" style="color:grey; padding-top:5px">favourdemo@gmail.com • (870) 949-0000 •
-                www.favourori.github.io • linkedin.com/in/favourori/ •github.com/orifavour</h4>
+            <h1>${data.firstname} ${data.lastname}</h1>
+            <h4 class="light" style="color:grey; padding-top:5px">
+            ${Object.keys(data.contact).map(function (key, index) {
+            return `
+                    <span class="">${data.contact[key]}</span>
+                `
+            })
+            .join(" |")}
+            </h4>
             <h4 class="h4">TECHNICAL SKILLS</h4>
             <div class="offset-2">
-                <p class="p-ma">
-                    <strong>Programming language: </strong>Java, Swift, Python, Javascript, PHP, Ruby, Rails
-                </p>
-                <p class="p-ma">
-                    <strong>Programming language: </strong>Java, Swift, Python, Javascript, PHP, Ruby, Rails
-                </p>
-                <p class="p-ma">
-                    <strong>Programming language: </strong>Java, Swift, Python, Javascript, PHP, Ruby, Rails
-                </p>
-                <p class="p-ma">
-                    <strong>Programming language: </strong>Java, Swift, Python, Javascript, PHP, Ruby, Rails
-                </p>
+            ${data.skills.details.map(function (key) {
+                return `
+                    <p class="p-ma">
+                    <strong>${key.type}: </strong> ${key.items.map(function (key) {
+                    return `${key}`;
+                })
+                 .join(", ")}`;
+            })
+            .join("</p>")}
             </div>
             <h4 class="h4">PERSONAL PROJECTS</h4>
             <div class="offset-2">
+            ${data.github_projects.items.map(function(key) {
+                return `
                 <div class="p-ma">
-                    <p>
-                        <strong class="title">Bethel Church (App Backend) iOS App Admin</strong>
-                    </p>
-                    <div class="offset-2 p">
-                        Desktop App (developed in Java) for managing & controlling the content of Bethel's Mobile
-                        AppTechnologies: Java, PHP, MySQL
-                    </div>
-                </div>
-                <div class="p-ma">
-                    <p>
-                        <strong class="title">Bethel Church (App Backend) iOS App Admin</strong>
-                    </p>
-                    <div class="offset-2 p">
-                        Desktop App (developed in Java) for managing & controlling the content of Bethel's Mobile
-                        AppTechnologies: Java, PHP, MySQL
-                    </div>
-                </div>
+                <p>
+                    <strong class="title">
+                        ${key["project_name"]} ${converter.makeHtml(key["tagline"]).replace(/<(\/)?p([^>]*)>/g, "")}
+                    </strong>
+                </p>
+                <div class="offset-2 p">
+                    ${key["description"][0]} Technologies: ${key["technology_used"]
+                .tools.map(function(item) {
+                    return item;
+                })
+            .join(", ")}
+                </div>`;
+            }).join("")}
+            </div>
             </div>
         </div>
         <div class="right_bar">
             <h4 class="h4">OTHER PROJECTS</h4>
-            <div class="offset-2">
+            ${data.other_projects.items.map(function(key) {
+                return `<div class="offset-2">
                 <div class="p-ma">
                     <p>
-                        <strong class="title">iOS App – Bethel Church, Magnolia</strong>
-                    <div class="offset-2 p">
-                        Designed an iOS app for Bethel Church, to reduce printing of bulletins, and to let members
-                        havedirect access to podcasts, upcoming events etc. Technologies: Swift, MySQL,PHP
-                    </div>
+                        <strong class="title">${key["headline"]}</strong>
                     </p>
-                </div>
-                <div class="p-ma">
-                    <p>
-                        <strong class="title">iOS App – Bethel Church, Magnolia</strong>
                     <div class="offset-2 p">
-                        Designed an iOS app for Bethel Church, to reduce printing of bulletins, and to let members
-                        havedirect access to podcasts, upcoming events etc. Technologies: Swift, MySQL,PHP
+                        ${key["points"][0]} Technologies: ${key["technology_used"]
+                    .tools.map(function(item) {
+                        return item
+                    })
+                    .join(", ")}
                     </div>
-                    </p>
                 </div>
-            </div>
+                </div>`;
+                })
+                .join("")}
             <h4 class="h4">PROFESSIONAL EXPERIENCE</h4>
             <div class="offset-2">
-                Software Developer, Southern Arkansas University, Magnolia, AR
-            </div>
+                <p>
+                    <strong class="title">${data.work_experience.items.map(function(key) {
+                        return `
+                            ${key["title"]}, ${key["organisation"]}, ${key["location"]}
+                            <span>${key["from"]} - ${key["to"]}</span>
+                            </strong>
+                            </p>
+                            <div class="offset-2 p">
+                            ${key["details"][0]} Technologies: ${key["technology_used"]
+                        .tools.map(function(item) {
+                            return item;
+                        })
+                    .join(", ")}
+                            </div>
+                            </div>`
+                    })}
             <h4 class="h4">INVOLVEMENTS</h4>
             <div>
                 <ul class="boxed-list offset-2 p">
-                    <li class="offset-2" style="text-decoration-style:disc">
-                        Volunteer, Magnolia Blossom, OCT 201
-                    </li>
+                ${data.involvement.organisations.map(function(key) {
+                    return `<li class="offset-2" style="text-decoration-style:disc"> ${key} </li>`;
+                })}
                 </ul>
             </div>
         </div>
@@ -245,23 +258,28 @@ const template2 = (data) => {
                 </thead>
                 <tbody>
                     <tr>
-                        <td>Bachelor of Science</td>
-                        <td>Computer Science</td>
-                        <td>Southern Arkansas University</td>
-                        <td>2020</td>
+                    ${data.education.schools.map(function(key) {
+                        return `<td>${key["degree"]}</td>
+                        <td>${key["major"]}</td>
+                        <td>${key["institution"]}</td>
+                        <td>${key["graduation_year"]}</td>
+                        `;
+                    })}
                     </tr>
                 </tbody>
             </table>
         </div>
-        <h4 class="h4">RESEARCH</h4>
-            <div class="p-ma mar">
+        <h4 class="h4">RESEARCH EXPERIENCE</h4>
+        ${data.research_experience.items.map(function(key) {
+            return `<div class="p-ma mar">
                 <p>
-                    <strong class="title">Web Hacking- National Collegiate Honors Council</strong>
+                    <strong class="title"${key["title"]}- ${key["organisation"]}></strong>
                 </p>
                 <div class="offset-2 p">
-                    Implemented encryption methods (MD5 and SHA-1)to secure a previously unsecure Web
+                    ${key["points"][0]}
                 </div>
-            </div>
+            </div>`
+        }).join("")}
     </div>
 </body>
 
